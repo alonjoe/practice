3548d770c8a47f871046ed57a46728bf2cbc8098
alonjoe <aalonjoe@gmail.com>
2022-04-06 21:38:31 +0900

    firebase패키지 설치완료

--- src/shared/firebase.js
+++ src/shared/firebase.js
@@ -1,3 +1,6 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+
 const firebaseConfig = {
   apiKey: "AIzaSyCrcsnA3sBodNKw3nv6pfrXxITNMjig_YU",
   authDomain: "practice-a5323.firebaseapp.com",
@@ -7,3 +10,8 @@ const firebaseConfig = {
   appId: "1:932797191664:web:78efc6b6d32e9eccebb935",
   measurementId: "G-ZHJCF988MG"
 };
+
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+
+export { auth }
