14a7d12d4af0016a7631c087e55f452e167bd6d0
alonjoe <aalonjoe@gmail.com>
2022-04-06 20:04:19 +0900

    쿠키로 헤더분기하기!! 나중에 리덕스로 관리할거라 공부용으로 기록하고 코드는 삭제.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,49 @@
 import React from "react";
 import styled from "styled-components";
 import { useHistory } from "react-router-dom";
+import { getCookie, deleteCookie } from "../shared/Cookie";
 
 import { Button, Grid, Text } from "../elements";
 
 const Header = () => {
   
   const history = useHistory();
-  
+  // 로그인 했는지 안했는지 확인! 했을때 안했을떄 상단메뉴 바뀌니까
+  const [is_login, setIsLogin] = React.useState(false);
 
+  React.useEffect(() => {
+    // 쿠키를 가져와요!
+    let cookie = getCookie('쿠키 이름 넣기!');
+    // 확인해봅시다!
+    console.log(cookie);
+    // 쿠키가 있으면?
+    if(cookie){
+        setIsLogin(true);
+    }else{
+        setIsLogin(false);
+    }
+  }, [])  // [] 안에 변경된 값이 없으면 함수 초기 1번만 실행. 있으면? 바뀔때마다 실행.
+
+  // 로그인일때 보여줄 상단
+  if (is_login) {
+    <React.Fragment>
+      <Grid is_flex padding="10px 10px">
+
+        <Grid>
+          <Text bold size="30px" margin="0px">instagram</Text>
+        </Grid>
+        
+        <Grid is_flex>
+          <Button text="내정보" _onClick={() => {}}></Button>
+          <Button text="알림" _onClick={() => {}}></Button>
+          <Button text="로그아웃" _onClick={() => {deleteCookie('login');}}></Button>
+        </Grid>
+
+      </Grid>
+    </React.Fragment>
+  }
+
+  // 로그인 안했을때 보여줄 상단
   return (
     <React.Fragment>
       <Grid is_flex padding="10px 10px">
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,23 +1,45 @@
 import React from "react";
 
+import { setCookie } from "../shared/Cookie";
 import { Grid, Text, Input, Button } from "../elements";
 
 const Login = () => {
+
+  const [id, setId] = React.useState("");
+  const [pw, setPw] = React.useState("");
+
+  // input에 들어온 아이디 저장
+  const changeId = (e) => {
+    setId(e.target.value);
+  }
+
+  // input에 들어온 비밀번호 저장
+  const changePw = (e) => {
+    setPw(e.target.value);
+  }
+
+  const login = () => {
+    setCookie("user_id", id, 3); // !!(함수명, 저장할값, 만료일(유효3일))!!
+    setCookie("user_pw", pw, 3);
+  }
 
   return (
     <React.Fragment>
       <Grid padding="12px">
         <Text bold size="20px">로그인</Text>
 
+        {/* 여기 들어가는 value의미는 value에 들어온값을 id, pw로 하겠다! */}
+        {/* 그렇게되면 여기들어가는 비밀번호는 value로 접근할수 있다! */}
         <Grid margin="20px 0">
-          <Input label="아이디" placeholder="아이디를 입력해주세요."></Input>
+          <Input value={id} onChange={changeId} label="아이디" placeholder="아이디를 입력해주세요."></Input>
         </Grid>
         
         <Grid margin="20px 0">
-          <Input type="password" label="비밀번호"placeholder="비밀번호를 입력해주세요."></Input>
+          <Input value={pw} onChange={changePw} type="password" label="비밀번호"placeholder="비밀번호를 입력해주세요."></Input>
         </Grid>
         
-        <Button margin="20px 0">로그인</Button>
+        {/* 로그인 버튼누르면 쿠키 저장!! */}
+        <Button margin="20px 0" _onClick={() => {login();}}>로그인</Button>  
       </Grid>
     </React.Fragment>
   )
