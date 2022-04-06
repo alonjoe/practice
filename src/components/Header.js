07c0789cf2316acc885b023418436b1fc6ed24d5
alonjoe <aalonjoe@gmail.com>
2022-04-06 20:07:02 +0900

    쿠키헤더분기 삭제(나중에 리덕스로 관리해기위해)

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,6 @@
 import React from "react";
 import styled from "styled-components";
 import { useHistory } from "react-router-dom";
-import { getCookie, deleteCookie } from "../shared/Cookie";
 
 import { Button, Grid, Text } from "../elements";
 
@@ -11,18 +10,6 @@ const Header = () => {
   // 로그인 했는지 안했는지 확인! 했을때 안했을떄 상단메뉴 바뀌니까
   const [is_login, setIsLogin] = React.useState(false);
 
-  React.useEffect(() => {
-    // 쿠키를 가져와요!
-    let cookie = getCookie('쿠키 이름 넣기!');
-    // 확인해봅시다!
-    console.log(cookie);
-    // 쿠키가 있으면?
-    if(cookie){
-        setIsLogin(true);
-    }else{
-        setIsLogin(false);
-    }
-  }, [])  // [] 안에 변경된 값이 없으면 함수 초기 1번만 실행. 있으면? 바뀔때마다 실행.
 
   // 로그인일때 보여줄 상단
   if (is_login) {
@@ -36,7 +23,7 @@ const Header = () => {
         <Grid is_flex>
           <Button text="내정보" _onClick={() => {}}></Button>
           <Button text="알림" _onClick={() => {}}></Button>
-          <Button text="로그아웃" _onClick={() => {deleteCookie('login');}}></Button>
+          <Button text="로그아웃" _onClick={() => {}}></Button>
         </Grid>
 
       </Grid>
